6d79564cb7a74440e4143ad69d97fbdc0302657f
Andrew Presnell <andrew_presnell@hotmail.com>
2017-09-29 10:19:47 -0700

    Fixed requires on liri.js

--- liri.js
+++ liri.js
@@ -1,15 +1,20 @@
-var keys = require("twitterKeys");
+var keys = require("./keys.js");
 var command = process.argv[2];
 
 //MY TWEETS
-
-
+if (command === "my-tweets") {
+	console.log("my-tweets works");
+}
 
 //SPOTIFY
-
+if (command === "spotify-this-song") {
+	console.log("spotify-this-song works");
+}
 
 
 //MOVIETHIS
-
+if (command === "movie-this") {
+	console.log("move-this works");
+}
 
 //BONUS OUTPUT DATA TO LOG.TXT
