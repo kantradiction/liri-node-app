c75cd6ad015898626391fb78e2cc2da795cfdf6d
Andrew Presnell <andrew_presnell@hotmail.com>
2017-09-29 14:57:15 -0700

    Fixed dowhatitsays function - made compatible with command functions

--- liri.js
+++ liri.js
@@ -1,18 +1,22 @@
+//---VARIABLES---
 var keys = require("./keys.js");
 var fs = require("fs");
 var command = process.argv[2];
 
-//MY TWEETS
-if (command === "my-tweets") {
+//---FUNCTIONS---
+function tweets() {
 	console.log("my-tweets works");
 }
 
-//SPOTIFY
-if (command === "spotify-this-song") {
+function spotify(song) {
 	console.log("spotify-this-song works");
 	var numOfWordsInSong = process.argv.length;
-	var songName = process.argv[3];
+	var songName = song;
 
+	if (song === undefined) {
+		songName = process.argv[3];
+	}
+	
 	// accept multiple words for song name
 	for(var i = 4; i < numOfWordsInSong; i++) {
 		songName = songName + " " + process.argv[i];
@@ -26,11 +30,14 @@ if (command === "spotify-this-song") {
 	console.log(songName);
 }
 
-//MOVIE THIS
-if (command === "movie-this") {
+function movie(movie) {
 	console.log("move-this works");
 	var numOfWordsInMovie = process.argv.length;
-	var movieName = process.argv[3];
+
+	var movieName = movie;
+	if(movie === undefined) {
+		movieName = process.argv[3];
+	}
 
 	// accept multiple words for song name
 	for(var i = 4; i < numOfWordsInMovie; i++) {
@@ -43,13 +50,44 @@ if (command === "movie-this") {
 	}
 
 	console.log(movieName);
-
 }
 
-//DO WHAT IT SAYS
-if (command === "do-what-it-says") {
+function doWhatItSays() {
+	var commandArg = "";
 	console.log("do-what-it-says works");
-
+	//retrieve command and command arg from file
+	
+	fs.readFile('random.txt', "utf8", function(error, data) {
+		dataArr = data.split(",");
+		command = dataArr[0];
+		commandArg = dataArr[1];
+		//run function based on command and command arg
+		checkCommand(commandArg);
+	});
 }
+
+function checkCommand(songOrMovie) {
+	//MY TWEETS
+	if (command === "my-tweets") {
+		tweets();
+	}
+
+	//SPOTIFY
+	if (command === "spotify-this-song") {
+		spotify(songOrMovie);
+	}
+
+	//MOVIE THIS
+	if (command === "movie-this") {
+		movie(songOrMovie);
+	}
+
+	//DO WHAT IT SAYS
+	if (command === "do-what-it-says") {
+		doWhatItSays();
+	}
+}
+
+checkCommand();
 
 //BONUS OUTPUT DATA TO LOG.TXT
