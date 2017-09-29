3b7151f662da3d6a634d53795062b823336840e4
Andrew Presnell <andrew_presnell@hotmail.com>
2017-09-29 10:10:04 -0700

    Initial File Creation

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '3k6HcokZPbBlO4bGYnITgTWkR',
+  consumer_secret: 'jjQXEyqbwpvJeb1ZT2wAHOa0lBnB8BYYLybFwob9dYAWE7PMvs',
+  access_token_key: '49927557-xV9On90kL5dMTiZlPtzOgvKC2IFac02bQ1jiHjtI8',
+  access_token_secret: 'c5u7obFM87euJkGRYjJykjDuMa131LcD0dju7rlytp9dE',
+}
+
+module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,15 @@
+var keys = require("twitterKeys");
+var command = process.argv[2];
+
+//MY TWEETS
+
+
+
+//SPOTIFY
+
+
+
+//MOVIETHIS
+
+
+//BONUS OUTPUT DATA TO LOG.TXT
